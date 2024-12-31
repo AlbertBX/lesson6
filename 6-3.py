ad49b3374fbaaaeb656852da611c1ce3e2953ce7
AlbertBX <avh1972@mail.ru>
2024-12-31 17:50:37 +0500

    Update 6-3.py

--- 6-3.py
+++ 6-3.py
@@ -3,16 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 driver = webdriver.Chrome()
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-#weiter = WebDriverWait (driver, 30)
+waiter = WebDriverWait(driver, 100)
 
-pictures = WebDriverWait (driver, 10).until(EC.presence_of_element_located ((By.CSS_SELECTOR, '#image-container.col-12.py-2')))
-img = driver.find_elements(By.ID, '#award')
-#'img[src="img/award.png"]'
+driver.get('https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
 
 
-print(img.get_attribute('src'))
+
+images=WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div#image-container.col-12.py-2 img#landscape")))
+img = driver.find_elements(By.CSS_SELECTOR, '#award')
+
+print(len(images))
+
+src = images(2).get_attribute("src")
+
+print(src)
 
 driver.quit()
